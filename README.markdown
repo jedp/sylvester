8c59c0e628a927d03583af95b11053e5f927d233
Jed Parsons <jedp@me.com>
2011-01-24 09:23:17 -0800

    Added common js exports wrapper, so now works with node

---
+++ README.markdown
@@ -0,0 +1,3 @@
+jcoglan's great javascript linear algebra library, wrapped for node.js.
+
+For more about sylvester, see http://sylvester.jcoglan.com/docs
---
+++ src/wrapper.js
@@ -0,0 +1,8 @@
+// expose contents via common js
+if (typeof exports != 'undefined' && exports != null) {
+    exports.Vector = Vector;
+    exports.Matrix = Matrix;
+    exports.Line = Line;
+    exports.Plane = Plane;
+}
+
